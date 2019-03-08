eab71c84c12aaebca91b43aded1d63a5e65810bf
Diana Levay <45174677+nanilevay@users.noreply.github.com>
2019-03-08 12:23:49 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ask user for input
+            Console.WriteLine("please input a height!");
+            // define height
+            string height = Console.ReadLine();
+            // turn into double 
+            float h = Convert.ToSingle(height);
+
+            // ask user for input
+            Console.WriteLine("please input a radius!");
+            //define radius
+            string radius = Console.ReadLine();
+            // turn into double
+            float r = Convert.ToSingle(radius);
+
+            // calculate the volume
+            double vol = Math.PI*Math.Pow(r, 2)*h;
+            // calculate the surface area
+            double surface = 2*Math.PI*r*(r+h);
+
+            // write the results
+            Console.WriteLine("volume do cilindro = {0} \narea da superficie do cilindro= {1}", vol, surface);
+
+        }
+    }
+}
