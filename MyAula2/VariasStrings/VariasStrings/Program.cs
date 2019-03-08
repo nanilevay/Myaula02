55b2bbeba0dc43954768086f7399590e708c3351
nanilev <dianuski99@gmail.com>
2019-03-08 10:10:49 +0000

    novas strings

--- MyAula2/VariasStrings/VariasStrings/Program.cs
+++ MyAula2/VariasStrings/VariasStrings/Program.cs
@@ -9,11 +9,20 @@ namespace VariasStrings
         {
 
             Console.OutputEncoding = Encoding.UTF8;
+            var o = 0;
+            var x = 1;
             string hello = "hello \n";
             string yolo = "yolo \n ";
             string fancyYolo = "\u13BD \u13C5 \u13DD \u13C5 \n";
             string goodbye = @"goodbye";
+            string nice = "nice " + 1 + "\n";
+            string ok = o + "k \n";
+            string valor = $"valor de o eh {o} \n";
+            string soma = $"{o} mais {x} eh igual a {o + x}";
+            string vars = String.Format("x eh {1}, o eh {0} \n", o, x);
             Console.WriteLine("{0}{1}{2}{3}", hello, yolo, fancyYolo, goodbye);
+            Console.WriteLine("{0}{1}{2}{3}{4}", nice, ok, valor, vars, soma);
+            Console.WriteLine(@"Verbatim o = {0}", o);
             Console.Read();
         }
     }
