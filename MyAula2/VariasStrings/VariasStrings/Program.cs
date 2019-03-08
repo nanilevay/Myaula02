4347240098e95e647523e61a7fa1ee511961ac62
nanilev <dianuski99@gmail.com>
2019-03-08 09:43:59 +0000

    strings ex02

---
+++ MyAula2/VariasStrings/VariasStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.UTF8;
+            string hello = "hello \n";
+            string yolo = "yolo \n ";
+            string fancyYolo = "\u13BD \u13C5 \u13DD \u13C5 \n";
+            string goodbye = @"goodbye";
+            Console.WriteLine("{0}{1}{2}{3}", hello, yolo, fancyYolo, goodbye);
+            Console.Read();
+        }
+    }
+}
