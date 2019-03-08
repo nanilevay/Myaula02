1647f645c16944a6abe165c67642c08458b4b5d2
Diana Levay <45174677+nanilevay@users.noreply.github.com>
2019-03-08 10:54:28 +0000

    Add files via upload

--- MyAula2/VariasStrings/VariasStrings/Program.cs
+++ MyAula2/VariasStrings/VariasStrings/Program.cs
@@ -9,6 +9,8 @@ namespace VariasStrings
         {
 
             Console.OutputEncoding = Encoding.UTF8;
+            double xx = 0.12345;
+            int ii = 18;
             var o = 0;
             var x = 1;
             string hello = "hello \n";
@@ -20,9 +22,13 @@ namespace VariasStrings
             string valor = $"valor de o eh {o} \n";
             string soma = $"{o} mais {x} eh igual a {o + x}";
             string vars = String.Format("x eh {1}, o eh {0} \n", o, x);
+            Console.WriteLine("Ex. 1");
             Console.WriteLine("{0}{1}{2}{3}", hello, yolo, fancyYolo, goodbye);
+            Console.WriteLine("Ex. 2");
             Console.WriteLine("{0}{1}{2}{3}{4}", nice, ok, valor, vars, soma);
             Console.WriteLine(@"Verbatim o = {0}", o);
+            Console.WriteLine("Ex. 3");
+            Console.WriteLine("{0:f2} {0:p1} {1:x} {1:c}", xx, ii);
             Console.Read();
         }
     }
